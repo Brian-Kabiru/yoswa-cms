e00fb61aefcfd80e241b9d6f0054a7d59e2d202a
Brian-Kabiru <bryankabiru01@gmail.com>
2026-01-23 21:27:49 +0300

    Initial commit

---
+++ schemaTypes/blog.ts
@@ -0,0 +1,45 @@
+// schemaTypes/blog.ts
+export default {
+  name: 'blog',
+  title: 'Blog Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'author',
+      title: 'Author',
+      type: 'string',
+    },
+    {
+      name: 'publishedAt',
+      title: 'Published At',
+      type: 'datetime',
+    },
+    {
+      name: 'mainImage',
+      title: 'Main Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    },
+  ],
+}
